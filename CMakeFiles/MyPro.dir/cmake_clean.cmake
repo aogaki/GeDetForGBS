eb38c0140b198d0d2de5354790df9e79b9c18d8b
Soiciro Aogaki <sohichiroh.aogaki@eli-np.ro>
2018-10-01 10:22:39 +0300

    .gitignore delete all unnecessary files

--- CMakeFiles/MyPro.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/MyPro"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/MyPro.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
